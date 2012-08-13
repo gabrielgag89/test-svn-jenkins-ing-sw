2d8b034a7b1d001c47ca4aa0d29b8dc434ce0219
desiree2008@gmail.com <desiree2008@gmail.com@c5e01e72-bca7-7789-ed37-6f60e81b109b>
2012-08-13 20:29:29 +0000

    Arreglo alcance de la clase SuscriptorGenerarFacturaPaciente

--- trunk/Clínica/src/observador/SuscriptorGenerarFacturaPaciente.java
+++ trunk/Clínica/src/observador/SuscriptorGenerarFacturaPaciente.java
@@ -1,10 +1,10 @@
-package observador;
+﻿package observador;
 
 import java.util.List;
 import java.util.ArrayList;
 import dtos.DTOFacturaPaciente;
 
-private class SuscriptorGenerarFacturaPaciente {
+public class SuscriptorGenerarFacturaPaciente {
    private static SuscriptorGenerarFacturaPaciente instancia;
    private List<ObservadorGenerarFacturaPaciente> observadores = new  ArrayList<ObservadorGenerarFacturaPaciente>();
    
